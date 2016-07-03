42b40c4c98eb68daf88d5ec78a93d2833a8e3f8c
zainmustafa <zmdhillo@gmail.com>
2016-07-03 23:32:14 +0500

    working todo list.

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App.</h1>'
+    template: '<div class="form-group">' +
+      '<h1>To-Do <small>List</small></h1>' +
+    '<form role="form">'+
+        '<input type="text" class="form-control" placeholder="Your Task" name="task">'+
+    '</form>'+
+    '<button type="button" class="btn btn btn-primary">Add</button>'+
+        '</div>'+
+        '<div></div>'+
+    '<ul> <li *ngFor="#todo of todoList"> {{todo}} <a href="#" class="close" aria-hidden="true"></a> </li> </ul>'
 })
-export class AppComponent { }
+export class AppComponent {
+    remove = function (i:number) {
+        debugger;
+        this.todoList.slice(i,1);
+    }
+    todoList : Array<any> = ['Pakistan', 'India', "Bangladesh"];
+}

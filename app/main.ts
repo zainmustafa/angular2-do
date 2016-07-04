4df1e3e6c36b2886e3c241804c7845302a4c8666
zainmustafa <zmdhillo@gmail.com>
2016-07-04 11:32:41 +0500

    Add Production mode.

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,10 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { disableDeprecatedForms, provideForms } from '@angular/forms';
+//import {enableProdMode} from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+//enableProdMode();
 bootstrap(AppComponent, [
   disableDeprecatedForms(),
   provideForms()
